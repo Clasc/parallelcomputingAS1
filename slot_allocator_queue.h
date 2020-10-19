af4214ed49d2da684f8a712a7490ba45c9b3096b
Christian Lascsak <christian.lascsak7@gmail.com>
2020-10-19 22:23:01 +0200

    add first analysation of slot_queue

--- slot_allocator_queue.h
+++ slot_allocator_queue.h
@@ -16,7 +16,9 @@ struct slot_allocator_queue
     slot_allocator_queue()
     {
         for (int i = 0; i < 10; ++i)
+        {
             slots.push(i);
+        }
     }
 
     int acquire_slot()
@@ -39,6 +41,7 @@ struct slot_allocator_queue
     {
         m.lock();
         slots.push(slot);
+        assertSlots(slot);
         m.unlock();
     }
 
@@ -46,4 +49,21 @@ private:
     int num_slots = 10;
     queue<int> slots;
     mutex m;
+
+    void assertSlots(int insertedVal) const
+    {
+        // slots are not allowed to be inserted twice. So the queue can never be larger than num_slots
+        assert(slots.size() <= num_slots);
+
+        // the newest inserted value should always be on the back of the queue
+        auto newestVal = slots.back();
+        assert(insertedVal == newestVal);
+
+        /* 
+            This code works all of the time.
+            However, the use of a queue, in combination with a mutex lock, has a negative impact on performance. 
+             - With assertions 40 sec.
+             - Without assertions 20 sec.
+        */
+    }
 };
